82c7f126e405a15f7512e20142b1aac6424da25f
Nancatcat <2710514756@qq.com>
2023-04-11 09:34:56 +0800

    4.11作业

---
+++ Project4.11/tese4_11.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int hour = 0;
+//	int minute = 0;
+//	int k = 0;
+//	scanf("%d:%d %d", &hour, &minute, &k);
+//	hour = (hour + (minute + k) / 60);
+//	k = (minute + k) % 60;
+//		printf("%02d:%02d\n",hour,k);
+//
+//	return 0;
+//
+//}
+//
+//
+//
+//int main()
+//{
+//	char a = 0;
+//	scanf("%c", &a);
+//	printf("%d\n", a);
+//
+//
+//	return 0;
+//}
