2bb1ab733c9a38c3785dceddd98509c4415f285a
Nancatcat <2710514756@qq.com>
2023-05-09 18:24:44 +0800

    5.8练习

---
+++ learn5_8/tese5_8.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//输出"Hello Nowcoder!"。开始你的编程之旅吧。
+#include <stdio.h>
+//
+//int main()
+//{
+//    printf("Hello Nowcoder!");
+//    return 0;
+//}
+//输出1234的8进制和16进制。
+//int main()
+//{
+//	printf("%#o,%#X", 1234, 1234);
+//	return 0;
+//}
+//输入一个整数1234，实现单独打印；
+//void print(int n)
+//{
+//	if (n <= 9)
+//	{
+//		printf("%d ", n);
+//	}
+//	else
+//	{
+//		print(n / 10);
+//		printf("%d ", n % 10);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print(n);
+//	return 0;
+//}
+//输入一个大写字母，输出小写字母；
+//int main()
+//{
+//	int c = 0;
+//	c = getchar();
+//	putchar(c + 32);//（大写字母与小写字母的asii码值相差32.所以加32）
+//
+//	return 0;
+//}
