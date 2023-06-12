06604069e1158ec90805f503c0f991daa3b1564a
Nancatcat <2710514756@qq.com>
2023-06-12 16:07:08 +0800

    6.12 练习

---
+++ Learn6_12/rese.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//计算最大公约数和最小公倍数之和；
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	while (scanf("%d%d", &n, &m) == 2)
+//	{
+//		int i = n;
+//		int j = m;
+//		int sum = 0;
+//		while (sum = i % j)
+//		{
+//			i = j;
+//			j = sum;
+//		}
+//		printf("%d\n",(n * m) / j + j);
+//
+//	}
+//	return 0;
+//}
+//打印空心正方形
+//int main()
+//{	int n = 0;
+//while (scanf("%d", &n) == 1)
+//{
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < n; j++)
+//		{
+//			if ((i == 0) || (i == n - 1) || (j == 0) || (j == n - 1))
+//			{
+//				printf("* ");
+//			}
+//			else
+//			{
+//				printf("  ");
+//			}
+//		}
+//		printf("\n");
+//	}
+//	}
+//	return 0;
+//}
