a0b6ec47dea7800ebb5075f606baf167030995bf
Nancatcat <2710514756@QQ.COM>
2023-06-19 16:50:13 +0800

    6.19练习

--- tese.c
+++ tese.c
@@ -1,218 +1,193 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-//#define OFFSETOF(type,name) (size_t)&(((type*)a)->name)
-//struct s
-//{
-//	char s;
-//	int g;
-//	char j;
-//};
-//#define OFFSETOF(type,name) (size_t)&(((type*)0)->name)
-//struct s
-//{
-//	char s;
-//	int g;
-//	char j;
-//};
 //int main()
 //{
-//	struct s j= { 0 };
-//	printf("%d\n", OFFSETOF(struct s ,s ));
-//	printf("%d\n", OFFSETOF(struct s ,g));
-//	printf("%d\n", OFFSETOF(struct s ,j));
+//	int arr1[10] = { 1,2,3,4,4,3,2,1,5,9 };
+//	int arr2[10] = { 0 };
+//	for (int i = 0; i < 10; i++)
+//	{
+//		arr2[i] = 1;
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j <= 10 - 1; j++)
+//		{
+//			if (arr1[i] == arr1[j + 1])
+//			{
+//				arr2[j] = 2;
+//			}
+//			if (arr1[i] == arr1[j])
+//			{
+//				arr2[j] = 2;
+//			}
+//
+//		}
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		if (arr2[i] == 1)
+//		{
+//			printf("%d ", arr1[i]);
+//		}
+//
+//	}
 //	return 0;
+//}
+//void loof(int* arr1, int* arr2)
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 10 - 1; j++)
+//		{
+//			if (arr1[i] == arr1[j])
+//			{
+//				if (i == j)
+//				{
+//					;
+//				}
+//				else
+//				{
+//					arr2[j] = 1;
+//				}
+//			}
+//		}
+//	}
 //}
 //int main()
 //{
-//	int n = 0;
-//	while (scanf("%d", &n)==1)
+//	int arr1[10] = { 1,2,3,4,5,3,2,1,4,6 };
+//	int arr2[10] = { 0 };
+//	loof(arr1, arr2);
+//	for (int i = 0; i < 10; i++)
 //	{
-//		int i = 0;
-//		for (i = 0; i < n; i++)
+//		if (arr2[i] ==0)
 //		{
-//			int j = 0;
-//			for (j = 0; j < n-i; j++)
-//			{
-//				printf("  ");
-//			}
-//			for (j = 0; j <= i; j++)
-//			{
-//				printf("*");
-//			}
-//			printf("\n");
+//			printf("%d ", arr1[i]);
 //		}
-//		for (i = 0; i < n + 1; i++)
+//
+//	}
+//	return 0;
+//}
+//void loot(int arr[], int* dog1, int* dog2, int sz)
+//{
+//	int ret = 0;
+//	for (int i = 0; i < sz; i++)
+//	{
+//		ret ^= arr[i];
+//	}
+//	int pow = 0;
+//	for (pow = 0; pow < 32; pow++)
+//	{
+//		if (((ret>> pow) & 1) == 1)
 //		{
-//			int j = 0;
-//			for (j = 0; j <i; j++)
-//			{
-//				printf("  ");
-//			}
-//			for (j = 0; j < n + 1-i; j++)
-//			{
-//				printf("*");
-//			}
-//			printf("\n");
+//			break;
 //		}
 //	}
+//	for (int i = 0; i < sz; i++)
+//	{
+//		if (((arr[i] >> pow) & 1 )== 1)
+//		{
+//			*dog1 ^= arr[i];
+//		}
+//		else
+//		{
+//			{
+//				*dog2 ^= arr[i];
+//			}
+//		}
+//	}
+//	printf("%d %d", *dog1, *dog2);
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,1,2,3,4,6 };
+//	int dog1 = 0;
+//	int dog2 = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	loot(arr, &dog1, &dog2, sz);
+//	return 0;
+//}
+#include<stdlib.h>
+#include<assert.h>
+//int Atoi(char* p1)
+//{
+//	//空指针
+//	assert(p1);
+//	int flang = 1;
+//	int  n = 0;
+//	//开头空格
+//	while (*p1 == ' ')
+//	{
+//		p1++;
+//	}
+//	//判断正负
+//	if (*p1 == '-')
+//	{
+//		flang = -1;
+//		p1++;
+//	}
+//	if (*p1 == '+')
+//	{
+//		flang = 1;
+//		p1++;
+//	}
+//	//转换
+//	while (*p1 != '\0')
+//	{
+//		if (isdigit(*p1))
+//		{
+//			n = (n * 10) + (*p1 - '0');
+//			if (n > INT_MAX)
+//			{
+//				n = INT_MAX;
+//			}
+//			if (n < INT_MIN)
+//			{
+//				n = INT_MIN;
+//			}
+//		}
+//		else
+//		{
+//			return n * flang;
+//		}
+//		p1++;
+//	}
+//	return n * flang;
+//}
+//
+//int main()
+//{
+//	char* p1 = "123456789";
+//	char* p2 = "-123456789";
+//	char* p3 = "-12345a789";
+//	char* p4 = "+12345a789";
+//	char* p5 = " ";
+//
+//	int a = Atoi(p1);
+//	int b = Atoi(p2);
+//	int c = Atoi(p3);
+//	int d = Atoi(p4);
+//	int e = Atoi(p5);
+//
+//
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	printf("%d\n", c);
+//	printf("%d\n", d);
+//	printf("%d\n", e);
+//
 //	return 0;
 //}
 
-//int main()
-//{
-//	FILE* file = fopen("file.txt", "wb");//打开一个文件，以二进制输出的方式打开;
-//	if (file == NULL)//判断文件指针是否接收成功
-//	{
-//		perror("file");
-//		return;
-//	}
-//	for (int i = 'a'; i < 'z'; i++)//循环写入
-//	{
-//		fputc(i, file);
-//	}
-//	fclose(file);//关闭文件
-//	file = NULL;//释放内存
-//
-//	return 0;
-//}
-//int main()
-//{
-//	FILE* file = fopen("file.txt", "rb");//打开一个文件，以二进制输入的方式打开;
-//	if (file == NULL)//判断文件指针是否接收成功
-//	{
-//		perror("file");
-//		return;
-//	}
-//	char arr;
-//	for (int i = 0; i < 27; i++)//循环输入
-//	{
-//		arr =fgetc(file);
-//		printf("%c ", arr);
-//	}
-//	fclose(file);//关闭文件
-//	file = NULL;//释放内存
-//
-//	return 0;
-//}
-//int main()
-//{
-//	FILE* file = fopen("file.txt", "wb");//打开一个文件，以二进制输入的方式打开;
-//	if (file == NULL)//判断文件指针是否接收成功
-//	{
-//		perror("file");
-//		return;
-//	}
-//	fputs("hello,world", file);
-//	fclose(file);//关闭文件
-//	file = NULL;//释放内存
-//
-//	return 0;
-//}
-//int main()
-//{
-//	FILE* file = fopen("file.txt", "rb");//打开一个文件，以二进制输入的方式打开;
-//	if (file == NULL)//判断文件指针是否接收成功
-//	{
-//		perror("file");
-//		return;
-//	}
-//	char arr[20];//创建数组接收从文件中输入的字符串
-//	int len = strlen("hello,world");
-//	fgets(arr,len+1, file);//输入字符串，fgtes的特性会导致'\0'也算一个字符，所以+1；
-//	printf("%s", arr);//打印
-//	fclose(file);//关闭文件
-//	file = NULL;//释放内存
-//
-//	return 0;
-//}
-//int main()
-//{
-//	FILE* file = fopen("file.txt", "wb");//打开一个文件，以二进制输入的方式打开;
-//	if (file == NULL)//判断文件指针是否接收成功
-//	{
-//		perror("file");
-//		return;
-//	}
-//	struct s i = { 'a',100,'c' };//结构体初始化
-//	fprintf(file, "%c %d %c", i.arr, i.b, i.c);//输出到文件中
-//	fclose(file);//关闭文件
-//	file = NULL;//释放内存
-//
-//	return 0;
-//}
-//struct s//定义一个结构体
-//{
-//	char arr[10];
-//	int b;
-//	char c[10];
-//};
-//int main()
-//{
-//	FILE* file = fopen("file.txt", "rb");//打开一个文件，以二进制输入的方式打开;
-//	if (file == NULL)//判断文件指针是否接收成功
-//	{
-//		perror("file");
-//		return;
-//	}
-//	struct s i = { 0 };//结构体初始化
-//	fscanf(file, "%s %d %s", i.arr, &(i.b), i.c);
-//	printf("%s %d %s", i.arr, i.b, i.c);
-//	fclose(file);
-//	file = NULL;//释放内存
-//
-//	return 0;
-//}
-//int main()
-//{
-//	int n[7] = { 0 };
-//	int max = 0;
-//	int min =100;
-//	double sum = 0.00;
-//	int i = 0;
-//	for (i = 0; i < 7; i++)
-//	{
-//		scanf("%d", &n[i]);
-//	}
-//	for (i = 0; i < 7; i++)
-//	{
-//		if (max <= n[i])
-//		{
-//			max = n[i];
-//		}
-//		else if (min >= n[i])
-//		{
-//			min = n[i];
-//		}
-//		sum += n[i];
-//	}
-//	printf("%.2lf", (sum - max - min) / 5.00);
-//	printf("\n");
-//	return 0;
-//}
-//int main()
-//{
-//	int n = 0;
-//	int max = 0;
-//	int min = 100;
-//	double sum = 0.00;
-//	int i = 0;
-//	while (scanf("%d", &n) == 1)
-//	{
-//		i++;
-//		if (max <= n)
-//			max = n;
-//		
-//		if (min >= n)
-//			min = n;
-//		sum += n;
-//		if (i == 7)
-//		{
-//			printf("%.2lf\n", (sum - max - min) / 5.00);
-//			i = 0;
-//			max = 0;
-//			min = 0;
-//			sum = 0;
-//		}
-//	}
-//	return 0;
-//}
+#define SWAP_BIT(n) n=(((n&0x55555555)<<1)+((n&0xaaaaaaaa)>>1))
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	SWAP_BIT(n);
+	printf("%d\n", n);
+	return 0;
+}
